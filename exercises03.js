c1b5a918b3a8ecd57671ba018ee8fadcd09c72b1
arif05rachman <arif05rachman@gmail.com>
2019-08-07 03:28:15 +0700

    add median

--- exercises03.js
+++ exercises03.js
@@ -9,7 +9,7 @@ function cariMedian(arr) {
     }
     return median;
   }
-  
+
   // TEST CASES
   console.log(cariMedian([1, 2, 3, 4, 5])); // 3
   console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
